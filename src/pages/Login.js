695a9ee652132d5c94455af8bc2bcdbf315aeb6d
fasaldana <fernando_s_98@hotmail.com>
2025-01-15 01:47:51 -0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { fetchUsers } from "../api/usersService";
 
 const Login = () => {
   const {
@@ -9,6 +10,8 @@ const Login = () => {
     formState: { errors },
   } = useForm();
   const navigate = useNavigate();
+
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -19,30 +22,27 @@ const Login = () => {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("/users.json");
-      const { users } = await response.json();
-
+      const users = await fetchUsers();
       const user = users.find(
         (user) =>
           user.username === data.username && user.password === data.password
       );
 
       if (user) {
-        console.log("Login successful");
         localStorage.setItem("token", true);
         navigate("/");
       } else {
-        console.log("Invalid credentials");
-        // Show error message
+        setError("Invalid username or password.");
       }
     } catch (error) {
-      console.error("Error fetching users:", error);
+      console.error(error.message);
+      setError("An error occurred. Please try again later.");
     }
   };
 
   return (
     <div className="bg-backgd h-screen flex items-center justify-center">
-      <div className="bg-white flex flex-col w-2/6 p-5 shadow-2xl">
+      <div className="bg-white flex flex-col md:w-2/6 p-5 shadow-2xl">
         <img
           src="/images/pokeball.png"
           alt="Pokeball image"
@@ -55,7 +55,7 @@ const Login = () => {
         <form className="flex flex-col p-10" onSubmit={handleSubmit(onSubmit)}>
           <input
             className="border-b-2 focus:outline-none focus:border-b-4"
-            placeholder="username"
+            placeholder="Username"
             {...register("username", { required: true })}
           />
           {errors.username && (
@@ -73,6 +73,7 @@ const Login = () => {
           <button className="bg-primary p-2 mt-10 text-white" type="submit">
             Login
           </button>
+          {error && <span className="text-error mt-2">{error}</span>}
         </form>
       </div>
     </div>
